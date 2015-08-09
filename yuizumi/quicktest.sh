570ad3807827dcab8211627f682ce8c64c138908
Yusuke Izumi <yuizumi@y5i.org>
2015-08-09 21:38:59 +0900

    Support uppercase in quicktest.sh.

--- yuizumi/quicktest.sh
+++ yuizumi/quicktest.sh
@@ -5,6 +5,7 @@ prob_dir=${0%/*}/../problems
     python json2cp.py < ${prob_dir}/problem_$1.json
     echo 1
     echo "$1 $2"
-    echo "$3" | tr "p'\!.03bcefy2aghij4lmno 5dqrvz2kstuwx" \
+    echo "$3" | tr A-Z a-z \
+              | tr "p'\!.03bcefy2aghij4lmno 5dqrvz2kstuwx" \
                    "222222111111444444333333555555666666"
 ) | ./validate
