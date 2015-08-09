ac376c2ef09847b2778b135ac959d13f765183db
Yusuke Izumi <yuizumi@y5i.org>
2015-08-09 09:32:35 +0900

    Revert 9af146d.

--- yuizumi/board.h
+++ yuizumi/board.h
@@ -1,8 +1,6 @@
-#include <functional>
 #include <vector>
 
 enum Command {
-    kNone,
     kMoveE,
     kMoveW,
     kMoveSE,
@@ -11,29 +9,28 @@ enum Command {
     kRotateLeft,
 };
 
-enum Direction { kRight = +1, kLeft = -1 };
+enum Direction {
+    kRight = +1,
+    kLeft = -1,
+};
 
-struct Cell { int x, y; };
+struct Cell {
+    int x, y;
+};
 
 class Board {
 public:
-    Board(int w, int h);
+    bool get(const Cell& cell) const;
+    bool get(int x, int y) const;
 
-    bool get(const Cell& cell) const { return get(cell.x, cell.y); }
-    bool get(int x, int y) const { return states_[y][x]; }
-
-    void set(const Cell& cell, bool value) { set(cell.x, cell.y, value); }
-    void set(int x, int y, bool value) { states_[y][x] = value; }
-
-    void ClearRows();
+    void set(const Cell& cell, bool value);
+    void set(int x, int y, bool value);
 
 private:
-    int w_, h_;
     std::vector<std::vector<int>> states_;
 };
 
 class UnitSpec {
-public:
     UnitSpec(const Cell& pivot, const std::vector<Cell>& cells)
         : pivot_(pivot), cells_(cells) {
     }
@@ -67,9 +64,6 @@ public:
     void Invoke(Command command);
 
 private:
-    bool TestCells(std::function<Cell(const Cell&)> mover) const;
-    void MoveCells(std::function<Cell(const Cell&)> mover);
-
     Cell RotateCell(const Cell& cell, Direction d) const;
 
     Board* board_;
