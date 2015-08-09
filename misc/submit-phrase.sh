bd86bb1c490315572785915697466146d1333af1
Yusuke Izumi <yuizumi@y5i.org>
2015-08-09 21:36:07 +0900

    Make the ProblemID and Seed arguments to submit-phrase.sh.

--- misc/submit-phrase.sh
+++ misc/submit-phrase.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
 API_TOKEN=BoNF0I954Tpwq+EIm2YOYbPK+QO0+2qYJVfCcZLmuFk=
-curl -ssl3 --user :$API_TOKEN -X POST -H "Content-Type: application/json" -d "[{ \"problemId\": 0, \"seed\": 0, \"tag\": \"piyo\", \"solution\":  \"$1\" } ]" https://davar.icfpcontest.org/teams/79/solutions
-
+curl -ssl3 --user :$API_TOKEN -X POST -H "Content-Type: application/json" -d "[{ \"problemId\": $1, \"seed\": $2, \"solution\":  \"$3\" } ]" https://davar.icfpcontest.org/teams/79/solutions
